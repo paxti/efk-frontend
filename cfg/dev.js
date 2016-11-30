3a23d690ecf8a8ca15dd1eec6a25ebac4914b838
Iurii Tverezovskyi <iurii.tverezovskyi@gmail.com>
2016-11-30 08:46:50 -0600

    Add lightning css to Webpack build

--- cfg/dev.js
+++ cfg/dev.js
@@ -12,7 +12,8 @@ let config = Object.assign({}, baseConfig, {
   entry: [
     'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
     'webpack/hot/only-dev-server',
-    './src/index'
+    './src/index',
+    './node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css'
   ],
   cache: true,
   devtool: 'eval-source-map',
